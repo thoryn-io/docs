530b783b24231b2db9db6d3bb81fd5e967106e64
markbakker <bakker.mark@gmail.com>
2025-09-02 18:45:29 +0200

    Set basePath and assetPrefix for Next.js configuration
    This update adds `basePath` and `assetPrefix` to the Next.js configuration, enabling support for serving the app from a subdirectory. It prepares the app to operate correctly when deployed under a specific path.

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
+const repo = 'docs' // <-- change or remove
+
 const nextConfig: NextConfig = {
     output: 'export',
     images: { unoptimized: true }, // required for next/image
     trailingSlash: true,
+    basePath: `/${repo}`,
+    assetPrefix: `/${repo}/`,
 };
 
 export default nextConfig;
