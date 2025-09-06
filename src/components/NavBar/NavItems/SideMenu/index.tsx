43ec3c036f6c39eda195271e28cbf506a050ae78
Mark Bakker <bakker.mark@gmail.com>
2025-09-06 15:39:30 +0200

    Feature/side menu (#2)
    * Add SideMenu and Menu components to NavBar
    
    Introduced new SideMenu and Menu components to improve mobile navigation. Updated NavBar structure to include these components, enhancing accessibility and user experience.
    
    * Add close button with XMarkIcon in NavBar menu
    
    Replaced the SVG for the close button with the XMarkIcon from Heroicons for consistency and readability. Updated PopoverPanel attributes to improve code clarity and alignment. Adjusted class names for better structure and maintainability.
    
    * Remove unused imports and simplify NavBar components
    
    Eliminated redundant imports in Menu, NavBar, and SideMenu components to enhance code clarity and maintainability. This cleanup removes unused dependencies and improves overall readability.

---
+++ src/components/NavBar/NavItems/SideMenu/index.tsx
@@ -0,0 +1,8 @@
+
+import Menu from "@/components/NavBar/Popovers/Menu";
+
+export default function SideMenu() {
+    return (
+        <Menu label="Open menu"/>
+    )
+}
--- src/components/NavBar/Navigation/index.tsx
+++ src/components/NavBar/Navigation/index.tsx
@@ -1,10 +1,11 @@
 import { PropsWithChildren } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import {SideMenu} from "@/components/NavBar/NavItems";
 
 export default function Navigation({ children }: PropsWithChildren) {
     return (
-        <div className="sticky top-0 z-50 w-full bg-purple-50 px-16">
+        <div className="sticky top-0 z-50 w-full bg-purple-50 px-1">
             <header>
                 <div className="relative flex h-12 items-center justify-between gap-4 text-sm duration-150 ease-linear">
                     <Link href="/">
@@ -27,8 +28,36 @@ export default function Navigation({ children }: PropsWithChildren) {
                             </ul>
                         </nav>
                     </div>
+                    <div className="ms-auto flex shrink-0 items-center gap-4">
+                        <div className="hidden xl:block">
+                            <div data-headlessui-state="">
+                                <div className="relative">
+                                    <button
+                                        className="flex items-center gap-1 font-semibold text-purple-800 hover:text-purple-500 focus-visible:text-purple-500 aria-current-page:text-purple-500"
+                                        type="button" aria-expanded="false" data-headlessui-state=""
+                                        id="headlessui-popover-button-«r30Rr9l6»"
+                                        aria-controls="headlessui-popover-panel-«r30R1r9l6»">
+                                        <span>Discover Proton</span>
+
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        <span
+                            className="h-auto w-0 flex-none border-r border-purple-200 max-[88em]:hidden my-1 self-stretch"></span>
+                        <div className="hidden sm:block"><a rel="noopener noreferrer"
+                                                            href="https://account.proton.me/mail/signup?ref=pme_lp_b2c_mail_menu"
+                                                            className="inline-flex shrink-0 items-center justify-center rounded-full text-center font-semibold outline-none transition-[color,background-color,border-color,box-shadow] duration-150 ease-out bg-purple-500 text-white ring-purple-800 ring-offset-white hocus:bg-purple-700 hocus:text-white no-underline ring-offset-2 focus-visible:ring motion-safe:active:translate-y-px py-1 px-3 text-sm gap-1 max-sm:w-full">Get
+                            Proton Mail</a></div>
+                        <div className="hidden md:block"><a rel="noopener noreferrer"
+                                                            href="https://account.proton.me/mail"
+                                                            className="inline-flex shrink-0 items-center justify-center rounded-full text-center font-semibold outline-none transition-[color,background-color,border-color,box-shadow] duration-150 ease-out text-purple-500 ring-purple-800 ring-offset-white hocus:bg-purple-700 hocus:text-white bg-transparent no-underline ring-offset-2 focus-visible:ring motion-safe:active:translate-y-px py-1 px-3 text-sm gap-1 max-sm:w-full">Sign
+                            in</a></div>
+                    </div>
+                    <SideMenu/>
                 </div>
             </header>
+
         </div>
     );
 }
